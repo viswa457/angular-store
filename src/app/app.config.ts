1192474c305b511983da4c280dc767634cc1e50c
Viswa <viswa457@gmail.com>
2024-12-02 12:42:59 -0600

    fix: add http clinet provider

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routeConfig } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(), 
     provideRouter(routeConfig), provideClientHydration()
   ]
 };
